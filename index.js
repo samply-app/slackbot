5e5f4e5ac0bda421715116c67af332954a34bb55
Eric Schirtzinger <eschirtzinger@gmail.com>
2020-01-19 16:58:03 -0700

    Added Slack message sending support

--- index.js
+++ index.js
@@ -1,15 +1,58 @@
+const request = require('request');
 const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('message');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    // Get inputs
+    const message = core.getInput('message') || "Fallback";
+    const slackToken = core.getInput('slack-token');   
+    // const slackToken = "xoxb-570934843381-899781945906-SuPQ46qAmej8RKwdKUDJYLXN"
+    const slackBody = {
+        channel: "GS4751SRE",
+        text: message,
+        blocks: [
+            {
+                type: "section",
+                text: {
+                    type: "mrkdwn",
+                    text: message
+                }
+            },
+            // {
+            //     type: "actions",
+            //     elements: [
+            //         {
+            //             type: "button",
+            //             text: {
+            //                 type: "plain_text",
+            //                 text: "Review 🔧",
+            //                 emoji: true
+            //             },
+            //             url: "https://samply.app"
+            //         }
+            //     ]
+            // }
+        ]
+    }
+    tellSlack(JSON.stringify(slackBody), slackToken);
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    // console.log(`The event payload: ${payload}`);
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
+}
+
+function tellSlack(body, token) {
+    const options = {
+        method: 'POST',
+        url: 'https://slack.com/api/chat.postMessage',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+            'Authorization': `Bearer ${token}`
+        },
+        body,
+    };
+    request(options, function (error, response) {
+        if (error) throw new Error(error);
+        console.log(response.body);
+    });
 }
