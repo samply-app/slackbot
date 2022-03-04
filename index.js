e687e58d2a8585cf16e32d6eb24eb5e9d2bfd005
Eric Schirtzinger <eschirtzinger@gmail.com>
2022-03-04 14:05:14 -0700

    feat: refactor and pray

--- index.js
+++ index.js
@@ -2,12 +2,32 @@ const request = require('request');
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-// Inputs
-const TOKEN = core.getInput('token') || core.getInput('slack-token'); // slack-token is deprecated
-const CHANNEL = core.getInput('channel') || "#devops"
-
-const _rawActions = core.getInput('actions');
-const ACTIONS = _rawActions ? JSON.parse(_rawActions) : null;
+/**
+ * Makes the actual API request to Slack
+ * @param {object} body 
+ * @param {string} token 
+ */
+function postMessage(body, token) {
+  // Configure API request
+  const options = {
+    method: 'POST',
+    url: 'https://slack.com/api/chat.postMessage',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      'Authorization': `Bearer ${token}`
+    },
+    body: JSON.stringify(body),
+  };
+  // Make request
+  request(options, (error, response) => {
+    if (error) throw new Error(error);
+    // Read API response
+    const body = JSON.parse(response.body);
+    if (!body.ok) {
+      throw new Error(response.body.error);
+    }
+  });
+}
 
 /**
  * Maps GitHub username to Slack member ID
@@ -24,28 +44,38 @@ function getSlackID(username) {
   }
 }
 
-function postMessage(body) {
-  // Configure API request
-  const options = {
-    method: 'POST',
-    url: 'https://slack.com/api/chat.postMessage',
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-      'Authorization': `Bearer ${TOKEN}`
-    },
-    body: JSON.stringify(body),
-  };
-  // Make request
-  request(options, (error, response) => {
-    if (error) throw new Error(error);
-    // Read API response
-    const body = JSON.parse(response.body);
-    if (!body.ok) {
-      throw new Error(response.body.error);
-    }
-  });
+/**
+ * Builds an actions block from a provided array
+ * @param {array} actions 
+ * @returns 
+ */
+function getActionsBlock(actions) {
+  const elements = [];
+  for (let i = 0; i < actions.length; i += 1) {
+    const action = actions[i];
+    const button = {
+      type: "button",
+      text: {
+        type: "plain_text",
+        text: action.text,
+        emoji: true
+      },
+      url: action.url
+    };
+    if (action.style) button.style = action.style;
+    elements.push(button);
+  }
+  return {
+    type: "actions",
+    elements
+  }
 }
 
+// Inputs
+const TOKEN = core.getInput('token') || core.getInput('slack-token'); // slack-token is deprecated
+const CHANNEL = core.getInput('channel') || "#devops";
+const ACTIONS = core.getInput('actions') ? JSON.parse(core.getInput('actions')) : null;
+const MESSAGE = core.getInput('message');
 
 /**** EXTRACT DATA ****/
 const ghContext = github.context;
@@ -56,28 +86,18 @@ console.log(ghContext);
 // Workflow info
 const eventName = ghContext.eventName;
 const workflow = ghContext.workflow;
-const branch = ghPayload.ref.substring(ghPayload.ref.lastIndexOf('/') + 1);
 
 // User information
 const senderLogin = ghPayload.sender.login
 const senderAvatar = ghPayload.sender.avatar_url;
 const senderURL = ghPayload.sender.html_url;
 
-// Commit info (this may not exist)
-const commitURL = ghPayload.head_commit.url;
-const commitMessage = ghPayload.head_commit.message;
-const commitHash = ghPayload.head_commit.id.substring(0, 7);
-
-
 /**
- * Generates a generic context block.
- * Should work for all triggers and workflows.
+ * Generates the body of the API request
+ * @param {string} channel 
+ * @param {string} text 
  * @returns 
  */
-function getContextBlock() {
-  //
-}
-
 function getBody(channel, text) {
   return {
     channel,
@@ -86,35 +106,43 @@ function getBody(channel, text) {
   }
 }
 
-const notificationText = ':tada: Integration successful'
+const notificationText = MESSAGE;
 
 const body = getBody(CHANNEL, notificationText);
 
 // Message body
-body.blocks.push({
-  "type": "section",
-  "text": {
-    "type": "mrkdwn",
-    "text": `*${notificationText}*\n\n${commitMessage}`,
-  }
-})
+if (eventName === 'push') {
+  const commitMessage = ghPayload.head_commit.message;
+  body.blocks.push({
+    "type": "section",
+    "text": {
+      "type": "mrkdwn",
+      "text": `*${notificationText}*\n\n${commitMessage}`,
+    }
+  })
+}
 
-// Workflow specifc context
+// Add workflow title
 body.blocks.push({
   "type": "context",
-  "elements": [
-    {
-      "type": "mrkdwn",
-      "text": `${workflow} · <${commitURL}|${commitHash}>`
-    }
-  ]
+  "elements": [{ "type": "mrkdwn", "text": workflow }]
 })
 
 // Event context
 function getContextString() {
   switch (eventName) {
-    case 'push':
-      return `<@${getSlackID(github.context.actor)}> · *${eventName}* on *${branch}*`;  
+    case 'push': {
+      const branch = ghPayload.ref.substring(ghPayload.ref.lastIndexOf('/') + 1);
+      const commitURL = ghPayload.head_commit.url;
+      const commitHash = ghPayload.head_commit.id.substring(0, 7);
+
+      return `<@${getSlackID(github.context.actor)}> · *${eventName}* <${commitURL}|${commitHash}> on *${branch}*`;
+    }
+    case 'pull_request': {
+      const title = ghPayload.pull_request.title;
+      const url = ghPayload.html_url;
+      return `<@${getSlackID(github.context.actor)}> · *${eventName}* <${url}|${title}>`;
+    }
     default:
       return 'Missing context...'
   }
@@ -137,25 +165,7 @@ body.blocks.push({
 
 /** If actions exist, add them in */
 if (ACTIONS) {
-  const elements = [];
-  for (let i = 0; i < ACTIONS.length; i += 1) {
-    const action = ACTIONS[i];
-    const button = {
-      type: "button",
-      text: {
-        type: "plain_text",
-        text: action.text,
-        emoji: true
-      },
-      url: action.url
-    };
-    if (action.style) button.style = action.style;
-    elements.push(button);
-  }
-  body.blocks.push({
-    type: "actions",
-    elements
-  });
+  body.blocks.push(getActionsBlock(ACTIONS));
 }
 
-postMessage(body)
+postMessage(body, TOKEN);
